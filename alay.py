0920512e8dbdcfc4b5f443c8584fca47b7fd19de
XinnCyber <138825222+XinnCyber@users.noreply.github.com>
2023-07-07 04:39:48 +0700

    Update alay.py
    TOOLS INI PUNYA XINN CYBER SITE

--- alay.py
+++ alay.py
@@ -3,6 +3,7 @@ from numpy import number
 import phonenumbers
 from phonenumbers import timezone, geocoder, carrier
 number = input("Masukan Nomor Target: ")
+print ="SC BY XINN"
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 car = carrier.name_for_number(phone, "en")
